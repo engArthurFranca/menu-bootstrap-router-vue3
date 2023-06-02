52c79c0497fd193f9d82e3a697484f85ea97cc76
Arthur França <eng.arthurfranca@gmail.com>
2023-06-02 20:41:48 -0300

    add routes to router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AppView from '../views/AppView.vue'
+import DocsView from '../views/DocsView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const routes = [
   {

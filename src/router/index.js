239d533f15dff7e724c6732146cd8eb2470ca3be
Arthur França <eng.arthurfranca@gmail.com>
2023-06-15 11:55:39 -0300

    add Web Hash History

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AppView from '../views/AppView.vue'
 import DocsView from '../views/DocsView.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
